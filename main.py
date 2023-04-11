c00f90b99f7c218f47ad505303b7d2681ba7a476
Shelby S <39410353+stshuf@users.noreply.github.com>
2023-04-11 18:24:29 -0400

    Update main.py
    General upgrades and improvements
    Also removed comments that were there for development

--- main.py
+++ main.py
@@ -52,17 +52,6 @@ def show_all_data():
 
 	return main
 
-@app.get("/get_user")
-def get_user(user_id: int = Query(None, title="User_id", description="User id as it appears in dataset.")):
-	get_user = [user_obj for key, user_obj in all_users.items() if user_obj["id"] == user_id]
-
-	if len(get_user) > 0:
-		user = get_user[0]
-	else:
-		return HTTPException(status_code=400, detail=f"User with ID does not exist {user_id}")
-
-	return user
-
 @app.post("/create_user/")
 def create_user(user: userObj):
 	## Need to set up a unique id so take the max amount we have now add one. 
@@ -114,40 +103,10 @@ def create_loan(loan: loanObj):
 
 	return new_loan
 
-@app.get("/get_schedule/{loan_id}", status_code=200)
-def get_loan_schedule(loan_id: int):
-	get_loan = [l for l in all_loans.values() if l['id'] == loan_id]
-	if not get_loan:
-		return HTTPException(status_code=404, detail=f"No loan found with that id.")
 
-	loan_data = get_loan[0]
-	schedule = utils.generate_amortization_schedule(loan_data["amount"], loan_data["rate"], loan_data["length"])
-	print(type(schedule))
-	data = {
-		"Month": schedule["month"],
-		"Remaining Balance": schedule["remaining_balance"],
-		"Monthly Payment": schedule["monthly_payment"]
-	}
-
-	return data
-
-@app.get("/get_summary/", status_code=200)
-def get_monthly_summary(loan_id: int, month_number: int):
-	get_loan = [l for l in all_loans.values() if l['id'] == loan_id]
-	if not get_loan:
-		return HTTPException(status_code=404, detail=f"No loan found with that id.")	
-	loan_data = get_loan[0]
-	
-	amortization_schedule = utils.generate_amortization_schedule(loan_data["amount"], loan_data["rate"], loan_data["length"])
-	get_amounts = utils.get_current_balances(loan_data, month_number)
-
-	return get_amounts 
-
-@app.get("/get_loans", status_code=200)
-def get_loans(user_id: Optional[int] = Query(None, title="User_id", description="User id as it appears in dataset."), 
+@app.get("/get_user", status_code=200)
+def get_user(user_id: Optional[int] = Query(None, title="User_id", description="User id as it appears in dataset."), 
 				name: Optional[str] = Query(None, title="Name", description="User Name")):
-	msg = 'ok'
-	user = ['']
 
 	if user_id is None and name is None: 
 		return HTTPException(status_code=404, detail=f"Please specify a user you'd like to search.")
@@ -156,6 +115,7 @@ def get_loans(user_id: Optional[int] = Query(None, title="User_id", description=
 		## Chose to do a partial match instead of an exact match. 
 		## People have trouble spelling so wanted to give them some slack. Also if there are families.
 		user = [u for u in all_users.values() if u["id"] == user_id and name.lower() in u["name"].lower()]
+	
 	else: 
 		if user_id is None: 
 			user = [u for u in all_users.values() if name.lower() in u["name"].lower()]
@@ -163,11 +123,53 @@ def get_loans(user_id: Optional[int] = Query(None, title="User_id", description=
 			user = [u for u in all_users.values() if u["id"] == user_id]
 
 	if len(user) > 1:
-		msg = "Multiple Users found with those parameters"
+		return HTTPException(status_code=404, detail=f"Multiple users found those parameters. {user}")
 	elif not user:
 		return HTTPException(status_code=404, detail=f"No users found with those parameters.")
 
-	return msg, user
+	return user
+
+
+@app.get("/get_schedule/{loan_id}", status_code=200)
+def get_loan_schedule(loan_id: int):
+	get_loan = [l for l in all_loans.values() if l['id'] == loan_id]
+	if not get_loan:
+		return HTTPException(status_code=404, detail=f"No loan found with that id.")
+
+	loan_data = get_loan[0]
+	schedule = utils.generate_amortization_schedule(loan_data["amount"], loan_data["rate"], loan_data["length"])
+
+	return schedule
+
+@app.get("/get_summary/", status_code=200)
+def get_month_summary(loan_id: int, month_number: int):
+	get_loan = [l for l in all_loans.values() if l['id'] == loan_id]
+	
+	if not get_loan:
+		return HTTPException(status_code=404, detail=f"No loan found with that id.")	
+	
+	loan_data = get_loan[0]
+	get_amounts = utils.get_current_balances(loan_data, month_number)
+
+	return get_amounts 
+
+
+@app.get("/get_loan")
+def get_loan(user_id: int = Query(None, title="User_id", description="User id as it appears in dataset."),
+				name: Optional[str] = Query(None, title="Name", description="User's Full Name")):
+
+	loans = []
+	if name is not None and user_id is None: 
+		user_id = [u["id"] for u in all_users.values() if name.lower() in u["name"].lower()]
+
+		if len(user_id) > 1:
+			return HTTPException(status_code=400, detail=f"Multiple Users with that name. {name}")
+		elif len(user_id) < 1:
+			return HTTPException(status_code=400, detail=f"User with that ID does not exist {user_id}")
+	
+	loans = [l for l in all_loans.values() if user_id == l["user_id"]]
+
+	return loans	
 
 @app.put("/share_loan", status_code=204)
 def share_loan(user_id: int, account_to_be_added: int):
@@ -180,8 +182,3 @@ def share_loan(user_id: int, account_to_be_added: int):
 	main_user_account['sharing_info_with'].append(account_to_be_shared["id"])
 
 	return main_user_account
-
-
-## Change monthly payments from in db to function that determines monthly payment 
-## round numbers to float in get current balances
-## get loans is returning usering not loan information
